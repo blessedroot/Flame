f91492c053ffa8a06432fd6c34f2d659b9e0d109
s0ckett <119443399+blessedroot@users.noreply.github.com>
2025-06-26 04:16:00 +0500

    доработка конфига

--- src/main/java/com/flame/api/config/FlameConfig.java
+++ src/main/java/com/flame/api/config/FlameConfig.java
@@ -1,11 +1,13 @@
 package com.flame.api.config;
 
-import org.bukkit.configuration.file.*;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.*;
 
 public class FlameConfig {
+
     private final File file;
     private FileConfiguration cfg;
 
@@ -21,15 +23,40 @@ public class FlameConfig {
     public void reload() {
         if (!file.exists()) {
             file.getParentFile().mkdirs();
-            try (InputStream is = getClass().getResourceAsStream("/" + file.getName());
-                 FileOutputStream fos = new FileOutputStream(file)) {
-                if (is != null) { byte[] buf = is.readAllBytes(); fos.write(buf); }
+            try (InputStream is = getClass().getResourceAsStream("/" + file.getName())) {
+                if (is != null) {
+                    try (OutputStream os = new FileOutputStream(file)) {
+                        byte[] buffer = new byte[1024];
+                        int length;
+                        while ((length = is.read(buffer)) > 0) {
+                            os.write(buffer, 0, length);
+                        }
+                    }
+                } else {
+                    file.createNewFile();
+                }
             } catch (IOException ignored) {}
         }
         cfg = YamlConfiguration.loadConfiguration(file);
     }
 
-    public void save() { try { cfg.save(file); } catch (IOException e) { e.printStackTrace(); } }
+    public void save() {
+        try {
+            cfg.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-    public FileConfiguration get() { return cfg; }
+    public FileConfiguration get() {
+        return cfg;
+    }
+
+    public boolean exists() {
+        return file.exists();
+    }
+
+    public File getFile() {
+        return file;
+    }
 }
