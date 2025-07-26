7d1d81f5788caab9d39e64bdd5bfe6fae8be5204
s0ckett <119443399+blessedroot@users.noreply.github.com>
2025-07-26 15:01:41 +0500

    поебота про которую я благополучно забыл

--- src/main/java/com/flame/api/scoreboard/type/ScoreboardType.java
+++ src/main/java/com/flame/api/scoreboard/type/ScoreboardType.java
@@ -1,12 +1,12 @@
-package com.flame.api.scoreboard.type;
+//  package com.flame.api.scoreboard.type;
 
-public enum ScoreboardType {
+// public enum ScoreboardType {
 
     /**
      * author : s0ckett
      * date : 09.01.25
      */
 
-    GLOBAL,        // Для всех игроков
-    PLAYER_SPECIFIC // Для конкретного игрока
-}
+ //   GLOBAL,        // Для всех игроков
+   // PLAYER_SPECIFIC // Для конкретного игрока
+//}
